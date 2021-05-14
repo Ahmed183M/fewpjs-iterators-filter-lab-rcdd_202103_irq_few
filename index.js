d1b4f9a112e673ebd86302b1c4772242712e593a
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 16:08:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function findMatching(drivers, text){
 
 function fuzzyMatch (drivers, text){
   let newArray = [];
+<<<<<<< HEAD
   newArray = drivers.filter(driver=>text.includes(driver[0]));
   return newArray;
 }
@@ -15,5 +16,14 @@ function fuzzyMatch (drivers, text){
 function matchName (drivers, text){
   let newArray = [];
   newArray = drivers.filter(driver=>driver.name==text);
+=======
+  newArray = drivers.filter(driver=>text.includes(driver.split("")));
+  return newArray;
+}
+
+function findMatching(drivers, text){
+  let newArray = [];
+  newArray = drivers.filter(driver=>driver.toLowerCase()==text.toLowerCase());
+>>>>>>> 92774c98299f37e3d91617f2fa9b87b469b7faa6
   return newArray;
 }
